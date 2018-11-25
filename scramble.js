49e288e9f97c977efeae4812f2b77824b8c7a937
Trevor Jex <jextrevor@gmail.com>
2018-11-24 17:23:48 -0700

    Add scramble effect

---
+++ scramble.js
@@ -0,0 +1,21 @@
+words = ["Learner","Thinker","Designer","Coder","Engineer"];
+index = 0;
+statictime = 2000;
+dynamictime = 500;
+function wait(){
+    setTimeout(fadeout,statictime);
+}
+function fadeout(){
+    document.getElementById("scramble").classList.add("hidden");
+    setTimeout(fadein,dynamictime);
+}
+function fadein(){
+    index++;
+    if(index >= words.length){
+        index = 0;
+    }
+    document.getElementById("scramble").innerHTML = `${words[index]}.`;
+    document.getElementById("scramble").classList.remove("hidden");
+    setTimeout(wait,dynamictime);
+}
+wait();
